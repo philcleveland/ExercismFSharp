59f970a0e8bf9e61f51c654eaa1c32af243b617f
phil <pdoh00@gmail.com>
2016-11-04 12:35:33 -0700

    Refactored for readability

--- bowling/BowlingAnswer.fs
+++ bowling/BowlingAnswer.fs
@@ -1,14 +1,20 @@
 module Bowling
     let newGame = []
 
+    let strikeScore x y = 10 + x + y
+    let spareScore x = 10 + x
+    let openFrameScore x y = x + y
     //note that the game rolls are returned in reverse order
     let roll (pins) (game) = pins::game
     let score game =
-      let rec calcScore (line:List<int>) score =
-        match line with
-        | 10::x::y::[] -> score + 10 + x + y //strike in 10th
-        | 10::x::y::tail -> calcScore (x::y::tail) score + 10 + x + y //strike
-        | x::y::z::tail when x + y = 10 -> calcScore (z::tail) score + 10 + z //spare
-        | x::y::tail -> calcScore (tail) score + x + y
-        | _ -> score
-      calcScore (game |> List.rev) 0 
+      //note that by using a Pattern Matching Function here 
+      //it is implicit that the calcScore function takes a curried list int
+      //use a pattern matching function when the match x with is the entirety
+      //of the function.
+      let rec calcScore curScore = function 
+        | 10::x::y::[] -> curScore + strikeScore x y
+        | 10::x::y::tail -> calcScore (curScore + strikeScore x y) (x::y::tail)
+        | x::y::z::tail when x + y = 10 -> calcScore (curScore + spareScore z) (z::tail)
+        | x::y::tail -> calcScore (curScore + openFrameScore x y) (tail) 
+        | _ -> curScore
+      game |> List.rev |> calcScore  0
